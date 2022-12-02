40cc83f3077875aa3e668d6b43b5c21bd24aca72
igorq937 <igorq937@gmail.com>
2022-12-02 20:08:47 -0300

    add dados geográficos dos distritos CHELSEA e WESTMINSTER

--- modules/map.js
+++ modules/map.js
@@ -1,9 +1,28 @@
+import mapData from './mapData.js';
+
 const width = 762;
 const hegiht = 758;
 const map = L.map('map', { crs: L.CRS.Simple });
 const bounds = [[0,0], [width, hegiht]];
 
+
 export function init() {
+    loadMapImage();
+    loadMapData();
+}
+
+function loadMapImage() {
     L.imageOverlay('./assets/map.png', bounds).addTo(map);
     map.fitBounds(bounds);
+}
+
+function loadMapData() {
+    mapData.forEach(value => {
+        const area = L.polygon(value.area, {color: value.colorArea}).addTo(map);
+        L.polyline(value.edges, {color: 'red'}).addTo(map);
+
+        value.nodes.forEach(node => {
+            L.marker(node).addTo(map);
+        })
+    })
 }
