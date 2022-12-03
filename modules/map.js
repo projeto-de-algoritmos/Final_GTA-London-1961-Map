f56bcebc8c7f58bf303c0d18ffc9bc4bcc78c685
Henrique Melo <hacmelo@gmail.com>
2022-12-03 19:33:40 -0300

    add menu add conceito de origem e destino add pop up com o nome dos nos

--- modules/map.js
+++ modules/map.js
@@ -1,12 +1,15 @@
 import mapData from './mapData.js';
 
 const width = 762;
-const hegiht = 758;
+const height = 758;
 const map = L.map('map', { crs: L.CRS.Simple });
-const bounds = [[0,0], [width, hegiht]];
+const bounds = [[0, 0], [width, height]];
+let from;
+let to;
 
 
 export function init() {
+    listenBtnEvents();
     loadMapImage();
     loadMapData();
 }
@@ -22,7 +25,61 @@ function loadMapData() {
         L.polyline(value.edges, {color: 'red'}).addTo(map);
 
         value.nodes.forEach(node => {
-            L.marker(node).addTo(map);
+            const marker = L.marker(node.pos).addTo(map);
+            addMarkerEvents(marker, node)
         })
     })
+}
+
+function addMarkerEvents(marker, node) {
+    marker.addEventListener('mouseover', (el) => {
+        L.popup(
+            el.latlng,
+            {
+                content: node.name,
+                offset: L.point(0, -30)
+            }
+        ).openOn(map);
+    })
+    .addEventListener('mouseout', () => {
+        map.closePopup();
+    })
+    .addEventListener('click', (el) => {
+        if (!from && !to) {
+            from = node;
+        } else if (from && !to && from === node) {
+            return window.alert("Selecione dois lugares diferentes!");
+        } else if (from && !to) {
+            to = node;
+        } else if (from && to) {
+            from = node;
+            to = null;
+        }
+        setTexts();
+    });
+}
+
+function listenBtnEvents() {
+    document.getElementById('route').addEventListener('click', () => calculateRoute());
+    document.getElementById('reset').addEventListener('click', () => resetRoute());
+}
+
+function calculateRoute() {
+    if (!from || !to) {
+        return window.alert("Selecione uma origem e um destino!")
+    }
+    // TODO mostrar edges
+}
+
+function resetRoute() {
+    to = null;
+    from = null;
+    setTexts();
+}
+
+function setTexts() {
+    const fromEl = document.getElementById("from");
+    const toEl = document.getElementById("to");
+    fromEl.innerText = from?.name || '-';
+    toEl.innerText = to?.name || '-';
 }
